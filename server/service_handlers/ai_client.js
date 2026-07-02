193d0321ccc78a652f83512d53d5f2b5b0d06e95
lonemohsin33 <mohsin.lone@nyalazone.com>
2026-07-02 10:54:36 +0530

    added error logs for ai hit

--- server/service_handlers/ai_client.js
+++ server/service_handlers/ai_client.js
@@ -3,19 +3,40 @@ import axios from 'axios';
 const AI_URL = process.env.AI_URL || 'http://localhost:5000';
 
 export async function extractFirstPage(filePath) {
-    const { data } = await axios.post(
+    try {
+        console.log(filePath)
+      const { data } = await axios.post(
         `${AI_URL}/ai/extract-first-page`,
         { file_path: filePath },
         { timeout: 300000 }
-    );
-    return data;
-}
+      );
+  
+      return data;
+    } catch (err) {
+      console.log("AI URL:", AI_URL);
+      console.log("Status:", err.response?.status);
+      console.log("Response:", err.response?.data);
+      console.log("Message:", err.message);
+  
+      throw err;
+    }
+  }
 
 export async function processDocument(filePath) {
-    const { data } = await axios.post(
-        `${AI_URL}/ai/process-document`,
-        { file_path: filePath },
-        { timeout: 600000 }
-    );
-    return data;
+    try{
+
+        const { data } = await axios.post(
+            `${AI_URL}/ai/process-document`,
+            { file_path: filePath },
+            { timeout: 600000 }
+        );
+        return data;
+    }catch(err){
+        console.log("AI URL:", AI_URL);
+        console.log("Status:", err.response?.status);
+        console.log("Response:", err.response?.data);
+        console.log("Message:", err.message);
+  
+        throw err;
+    }
 }
